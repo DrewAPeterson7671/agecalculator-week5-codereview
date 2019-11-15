a3e4d031af380d91e6a1a3115c519cebdc527248
Student <example@example.com>
2019-11-15 09:19:30 -0800

    first test failing, working on it

--- __tests__/agecalc-backend.test.js
+++ __tests__/agecalc-backend.test.js
@@ -1,8 +1,8 @@
-import { ageEarthCalc } from '/./../src/agecalc-backend.js';
+import { ageEarthCalc } from './../src/agecalc-backend.js';
 
 describe('ageEarthCalc', () => {
   test('determine if the number entered is an number', () => {
-    let ageEarthCalc = ageEarthCalc('age');
+    let ageEarthCalcTest = ageEarthCalc('age');
     expect(ageEarthCalc).toEqual(false);
   });
 });
---
+++ src/agecalc-backend.js
@@ -0,0 +1,12 @@
+export { ageEarthCalc }
+
+
+// business logic
+
+let ageEarthCalc = function(input) {
+  if (typeof(input) !== typeof(number)) {
+    return false;
+  } else {
+    return true;
+  }
+}
