745b91bd3093783120ce0101f76135e3b4ff2699
Student <example@example.com>
2019-11-15 10:41:58 -0800

    take basic stepsto constructing an object

--- src/agecalc-backend.js
+++ src/agecalc-backend.js
@@ -2,18 +2,26 @@ export { ageEarthInputCalc }
 export { ageMercuryCalc }
 
 
+
 // business logic
 
-let ageEarthInputCalc = function(input) {
-  if (typeof(input) !== 'number') {
+const mercuryFactor = .24;
+const venusFactor = .62;
+const marsFactor = 1.88;
+const jupiterFactor = 11.86;
+
+let ageEarthInputCalc = function(inputAge) {
+  if (typeof(inputAge) !== 'number') {
     return false;
-  } else if (typeof(input) === 'number') {
+  } else if (typeof(inputAge) === 'number') {
     return true;
   } else {
     console.log('input type error');
   }
 }
 
-let ageMercuryCalc = function(input) {
-  return 38;
+function PlanetAge(inputAge) {
+  const mercuryFactor = .24;
+  this.earthAge = inputAge;
+  this.mercuryAge = inputAge / mercuryFactor;
 }
